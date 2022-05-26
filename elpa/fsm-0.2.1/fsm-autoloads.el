114fc42e2115af5b68e2e608fc7704907ff09273
Graeme <graemetinsdale@gmail.com>
2022-05-26 10:54:19 +0100

    Lots of changes...

--- elpa/fsm-0.2.1/fsm-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; fsm-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("fsm.el") (23144 25712 100205 642000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; fsm-autoloads.el ends here
